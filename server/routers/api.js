7ed3019aab7203a663361389d1db4a55966a73de
Mark-Sturdevant <Mark.Sturdevant@ibm.com>
2021-11-30 13:34:27 -0800

    Proxy and Pact tests split for calculator and converter consumer
    * Hypothetically the UI could use proxy to converter directly
    * And more importantly UI via proxy to calculator
    * Just pact tests via proxy, not doing actual UI work

--- server/routers/api.js
+++ server/routers/api.js
@@ -1,8 +1,22 @@
-const express = require('express');
 const proxy = require("express-http-proxy");
 
 const apiHost = process.env.API_HOST || 'localhost:3001';
+const calculatorHost = process.env.CALCULATOR_API_HOST || 'localhost:3002';
+const converterHost = process.env.CONVERTER_API_HOST || 'localhost:3003';
 
 module.exports = function(app){
+  
+  app.use('/api', proxy(calculatorHost, {
+    filter: (req) => { 
+      return Promise.resolve(req.url.startsWith('/calculator'))
+    }
+  }));
+
+  app.use('/api', proxy(converterHost, {
+    filter: (req) => { 
+      return Promise.resolve(req.url.startsWith('/converter'))
+    }
+  }));
+
   app.use('/api', proxy(apiHost));
 };
